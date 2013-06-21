fc2d65a4956fc5cc74a23a61669c521fefc2cae2
Michael Kapnick <michaelkapnick@gmail.com>
2013-06-21 11:32:38 -0400

    Created taco backend folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TacoBackend::Application.config.secret_token = 'd244843564936712a0e40bb8a5ce27a4e37676a0d421e81a89bffcc6032fb49431a0a1b6a5a3a13c37bf1dca946a75c1bf1fc84cc997f8896ae20c2b1b1af18f'
