17a344e0d872d3ce87025bb87cd8282e0cf61614
Michael Kapnick <michaelkapnick@gmail.com>
2013-07-08 10:58:20 -0400

    Front end stuff

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,4 @@
 class Option < ActiveRecord::Base
   attr_accessible :t1, :t10, :t11, :t12, :t13, :t14, :t15, :t16, :t17, :t18, :t19, :t2, :t20, :t21, :t22, :t23, :t24, :t25, :t26, :t27, :t28, :t29, :t3, :t30, :t4, :t5, :t6, :t7, :t8, :t9
+  belongs_to :venue
 end
