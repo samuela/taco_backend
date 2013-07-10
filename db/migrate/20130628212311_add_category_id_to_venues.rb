62ff1c06a8fcae7459cd94104911f5bf1e6d7eed
Michael Kapnick <michaelkapnick@gmail.com>
2013-07-10 12:24:09 -0400

    removed migrations that are not correct

--- db/migrate/20130628212311_add_category_id_to_venues.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryIdToVenues < ActiveRecord::Migration
-  def change
-    add_column :venues, :category_id, :integer
-  end
-end
--- db/migrate/20130704162438_fix_column_name_venue.rb
+++
@@ -1,8 +0,0 @@
-class FixColumnNameVenue < ActiveRecord::Migration
-  def up
-  	rename_column :venues, :time_of_day, :tags
-  end
-
-  def down
-  end
-end
